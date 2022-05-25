e89d23d8e3cb2f536227d1368f74dc06f20103af
mnmhbbb <mnmhb64@gmail.com>
2022-05-26 02:35:30 +0900

    fix: 폼 모달 오픈 시 백그라운드 position: fixed; 로 전체화면 적용

--- src/components/TodoForm/style.tsx
+++ src/components/TodoForm/style.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const FormBackground = styled.div`
   z-index: 1;
-  position: absolute;
+  position: fixed;
   top: 0;
   left: 0;
   background-color: #000;
