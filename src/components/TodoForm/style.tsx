e017ba984257e504f01eb902ff87be8415f2f2a6
mnmhbbb <mnmhb64@gmail.com>
2022-05-26 22:19:02 +0900

    fix: 폼 width 모바일 환경에 맞게 조정

--- src/components/TodoForm/style.tsx
+++ src/components/TodoForm/style.tsx
@@ -26,6 +26,10 @@ export const Container = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
 
+  @media screen and (max-width: 400px) {
+    width: 100vw;
+  }
+
   .header {
     display: flex;
     justify-content: space-between;
